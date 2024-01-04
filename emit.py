85cb1f7af4f74ad23113d8ca1946b9bebadbafd0
Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>
2024-01-04 15:13:56 +0100

    Make ping and listen url configurable

--- emit.py
+++ emit.py
@@ -1,10 +1,12 @@
 import time
+import os
 
 import requests
 
 
 if __name__ == '__main__':
+    ping_url = os.environ.get('PING_URL', 'http://localhost:8080/ping')
 
     while True:
-        requests.get('http://localhost:5000/ping')
+        requests.get(ping_url)
         time.sleep(1)
--- listen.py
+++ listen.py
@@ -1,11 +1,12 @@
-import json
+import os
 
 import sseclient
 
 
 if __name__ == '__main__':
+    listen_url = os.environ.get('LISTEN_URL', 'http://localhost:8080/listen')
 
-    messages = sseclient.SSEClient('http://localhost:5000/listen')
+    messages = sseclient.SSEClient(listen_url)
 
     for msg in messages:
         print(msg)  # call print(dir(msg)) to see the available attributes
